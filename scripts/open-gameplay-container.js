d1ddd73c20beab7f20fc716a35cc1265047158cc
morgan-iverson <miverson@vmware.com>
2023-03-24 16:50:20 -0700

    introduced colors, drop shadow

--- scripts/open-gameplay-container.js
+++ scripts/open-gameplay-container.js
@@ -1,10 +1,15 @@
 var open_gameplay_cont_id = "cont-category";
-var gameplay_cont_class = "cont-gameplay";
+var gameplay_cont_class = "card-gameplay";
 var gameplay_conts = document.getElementsByClassName(gameplay_cont_class);
 const OPEN_ATTRIBUTE = "open";
 const styles = `
 [open] {
-    flex: 7;
+    flex: 12;
+}
+
+.card-gameplay {
+    transition: flex 1s;
+    transition-timing-function: ease;
 }
 `
 
@@ -18,31 +23,51 @@ window.onload = () => {
 
     Array.prototype.forEach.call(gameplay_conts, (e) => {
         e.addEventListener("click", (e) => {
-            console.log(e.srcElement.id);
-            openGamePlayContainer(e.srcElement.id);
+            let gameplay_card_id = e.srcElement.id;
+            if(nullOrEmpty(gameplay_card_id)) {
+                console.error("Source Element ID is Null")
+                gameplay_card_id = getParentGamePlayCard(e.srcElement);
+                if(nullOrEmpty(gameplay_card_id)) return;
+            }
+            if(gameplay_card_id == open_gameplay_cont_id) return;
+            openGamePlayContainer(gameplay_card_id);
         })
     });
 }
 
 function openGamePlayContainer(source_element_id) {
-    if(source_element_id == open_gameplay_cont_id) return;
     removeAttribute(open_gameplay_cont_id, OPEN_ATTRIBUTE);
     addAttribute(source_element_id, OPEN_ATTRIBUTE);
 }
 
 function addAttribute(element_id, attribute) {
-    console.log("Opening []", element_id);
-    console.log(document.getElementById(element_id));
+    // console.log("Opening []", element_id);
     document.getElementById(element_id).setAttribute(attribute, "");
     open_gameplay_cont_id = element_id;
-    console.log("New Gameplay Container: []", element_id);
+    console.log("New Gameplay Container: [" + element_id + "]", document.getElementById(element_id));
 
 }
 
 function removeAttribute(element_id, attribute) {
     let element_to_close = document.getElementById(element_id);
     if(element_to_close.getAttribute(OPEN_ATTRIBUTE) == null) return;
-    console.log("Closing []", element_id);
+    // console.log("Closing []", element_id);
     document.getElementById(element_id).removeAttribute(attribute);
 }
 
+function nullOrEmpty(value) {
+    return (value == null || value == "");
+}
+
+
+function getParentGamePlayCard(child_element) {
+    // console.log("Looking for gameplay card...")
+    let node = child_element.parentNode;
+    while (node) {
+        if (node.classList.contains(gameplay_cont_class)) {
+            return node.id;
+        }
+        node = node.parentNode;
+    }
+    return null;
+};
